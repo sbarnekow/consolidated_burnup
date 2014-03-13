e6823b4292d795385d09c8ac7847e0b81e398ce3
Sarah Barnekow <mellamopepe@gmail.com>
2014-03-12 17:27:12 -0700

    changed shadowfile to reflect mql via date

--- shadowfile.rb
+++ shadowfile.rb
@@ -3,20 +3,20 @@ require 'json'
 require 'uri'
 require 'net/http'
 
-# date_array = [DateTime.new(2014, 1, 10)]
+date_array = [DateTime.new(2014, 1, 10)]
 
-# def get_dates(array_of_dates)
-#   new_date = array_of_dates[-1] + 5
+def get_dates(array_of_dates)
+  new_date = array_of_dates[-1] + 5
 
-#   if new_date < (DateTime.new(2014, 1, 23))
-#     array_of_dates.push(new_date)
-#     get_dates(array_of_dates)
-#   else
-#     array_of_dates.push(DateTime.new(2014, 1, 23))
-#     return array_of_dates.sort!
-#   end
+  if new_date < (DateTime.new(2014, 1, 23))
+    array_of_dates.push(new_date)
+    get_dates(array_of_dates)
+  else
+    array_of_dates.push(DateTime.new(2014, 1, 23))
+    return array_of_dates.sort!
+  end
 
-# end
+end
 
 
 def render_chart(render_this)
@@ -42,45 +42,49 @@ def render_chart(render_this)
 end
 
 def retrieve_data
-    #below is stubbed data
-    # date = "2014-01-10" #this will eventually be date_array
-    # project_array = ["cc", "cc1"] #this will eventually be parameters['projects']
-    
-    # future_date_array = []
+    # below is stubbed data
+    date = '"2014-01-10"' #this will eventually be date_array
+    project_array = ["cc", "dvip", "bid"] #this will eventually be parameters['projects']
+    query = 'SELECT project, SUM("Story Points") WHERE type=Story AND SimpleStatus = Done AND "Date Accepted" <= '
+    whole_query = query + date
 
-    # project_array.each do |project|
+    future_date_array = []
+
+    project_array.each do |project|
       
-    #   uri = URI.parse("http://sarah:p@localhost:8080/api/v2/projects/#{project}/cards/execute_mql.json")
+      uri = URI.parse("http://sbarnekow:p@localhost:8080/api/v2/projects/#{project}/cards/execute_mql.json")
 
-    #   http = Net::HTTP.new(uri.host, uri.port)
-    #   request = Net::HTTP::Get.new(uri.request_uri)
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new(uri.request_uri)
       
-    #   #stubbed MQL
-    #   request.form_data = {:mql => 'SELECT project, SUM("Story Points") WHERE type=Story AND SimpleStatus = Done AND "Date Accepted" <= "1/1/2014"'}
+      #stubbed MQL
+      request.form_data = {:mql => whole_query}
       
-    #   #MQL will take a string, interpolation isnt working :( 
-    #   response = http.request(request)   
+      #MQL will take a string, interpolation isnt working :( 
+      response = http.request(request)   
 
-    #   #creates the structure for the required dataset   
-    #   future_date_array.push(response.body)
-    # end
+      #creates the structure for the required dataset   
+      future_date_array.push(response.body.to_json)
 
-    #get a set of data for each project, using date array
-    #push that set of date to the corresponding arrays within data_array
-    #for project 1 would go to [1][1], [2][1], [3][1], [4][1] 
-    #project 2 would go to [1][2], [2][2], [3][2], [4][2]
+    end
 
-    #dates and values will only be strings
+    puts future_date_array
+    # get a set of data for each project, using date array
+    # push that set of date to the corresponding arrays within data_array
+    # for project 1 would go to [1][1], [2][1], [3][1], [4][1] 
+    # project 2 would go to [1][2], [2][2], [3][2], [4][2]
+
+    # dates and values will only be strings
 
 
-    return data_array = [
-            ['Day', 'Amount'],
-            [DateTime.parse('2014/1/09'),  0],
-            [DateTime.parse('2014/1/10'),  1000],
-            [DateTime.parse('2014/1/11'),  1170],
-            [DateTime.parse('2014/1/12'),  1220],
-            [DateTime.parse('2014/1/13'),  1300]
-    ]
+    # return data_array = [
+    #         ['Day', 'Amount'],
+    #         [DateTime.parse('2014/1/09'),  0],
+    #         [DateTime.parse('2014/1/10'),  1000],
+    #         [DateTime.parse('2014/1/11'),  1170],
+    #         [DateTime.parse('2014/1/12'),  1220],
+    #         [DateTime.parse('2014/1/13'),  1300]
+    # ]
 end
 
 
