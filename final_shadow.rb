3a6104ab0088d90221dba92cd06185e84cbb6442
Sarah Barnekow <mellamopepe@gmail.com>
2014-04-08 16:45:13 -0700

    final_shadow.rb now renders data correctly in test.html

--- final_shadow.rb
+++ final_shadow.rb
@@ -34,7 +34,7 @@ def compose_date_array
 end
 
 compose_date_array
-# pp @date_array
+pp @date_array
 
 def add_date_to_query
 	dates = @date_array.sort!
@@ -111,110 +111,61 @@ def consolidate_scope_data
 
 	final_data_structure << total_scope_queries.transpose.map! {|x| x.reduce(:+)}
 	final_data_structure << completed_scope_queries.transpose.map! {|x| x.reduce(:+)}
-	pp final_data_structure
-end
 
-consolidate_scope_data
+	return final_data_structure
+end
 
 def construct_final_array
+	header = ["Date", "Total Scope", "Completed Scope"]
+	@date_array.map! {|date| date.to_s.split('-').collect{|x| x.to_i}}
+	project_data = consolidate_scope_data.transpose
+	dates_and_data = @date_array.zip(project_data)
+	dates_and_data.each {|x| x.flatten!}
+	dates_and_data.unshift(header)
+	return dates_and_data
 end
 
+def render_chart
+	%{
 
-
-
-# def construct_final(dates, query_results)
-# 	query_categories = [:total_scope_query, :completed_scope_query]
-	
-# 	#splits the array in two, one for :total_scope_query and one for :completed_scope_query
-# 	# eg. [[34, 262, 505, 560, 560, 560], [12, 107, 206, 227, 227, 227]]
-# 	queries_by_date = query_results.each_slice(dates.length).to_a
-	
-# 	header = ["Date", "Total Scope", "Completed Scope"]
-
-# 	# splits date values out into arrays to be processed by javascript
-# 	# so that the values can be mapped to them
-# 	# eg. [[2014, 2, 1],
-# 	#  [2014, 2, 16],
-# 	#  [2014, 3, 3],
-# 	#  [2014, 3, 18],
-# 	#  [2014, 4, 2],
-# 	#  [2014, 4, 17]]
-
-# 	initial_arr = dates.map{|date| date.to_s.split('-').collect{|x| x.to_i}}
-
-# 	# now transposing :total_scope_query and :completed_scope_query to prepare each value to be mapped to the correct date
-# 	# [[34, 12], [262, 107], [505, 206], [560, 227], [560, 227], [560, 227]]
-# 	queries_arr = queries_by_date.transpose
-
-# 	# adds date_array to the query results array to be mapped in the way the google maps API takes them
-# 	# eg. [[[2014, 2, 1], [34, 12]],
-# 	#  [[2014, 2, 16], [262, 107]],
-# 	#  [[2014, 3, 3], [505, 206]],
-# 	#  [[2014, 3, 18], [560, 227]],
-# 	#  [[2014, 4, 2], [560, 227]],
-# 	#  [[2014, 4, 17], [560, 227]]]
-# 	final_arr = initial_arr.zip(queries_arr)
-
-# 	# flattens the array into a google maps readable format
-# 	# eg [[2014, 2, 1, 34, 12],
-# 	#  [2014, 2, 16, 262, 107],
-# 	#  [2014, 3, 3, 505, 206],
-# 	#  [2014, 3, 18, 560, 227],
-# 	#  [2014, 4, 2, 560, 227],
-# 	#  [2014, 4, 17, 560, 227]]
-# 	final_arr.each do |x|
-# 		x.flatten!
-# 	end
-
-# 	# adds the header to the front of the array
-# 	final_arr.unshift(header)
-	
-# 	return final_arr
-# end
-
-# final_data = construct_final(dates, scope)
-
-# def render_chart(arr)
-# 	%{
-
-#       <div id="chart_div" style="width: 900px; height: 500px;"></div>
+      <div id="chart_div" style="width: 900px; height: 500px;"></div>
       
-#       <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-#       <script type="text/javascript">
-#         google.load("visualization", "1", {packages:["corechart"]});
-#         google.setOnLoadCallback(drawChart);
+      <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+      <script type="text/javascript">
+        google.load("visualization", "1", {packages:["corechart"]});
+        google.setOnLoadCallback(drawChart);
       
-#       	function formatDates(){
-#       		debugger;
-#       		var dataArray = #{arr};
+      	function formatDates(){
+      		debugger;
+      		var dataArray = #{construct_final_array};
 
-#       		 for(var i=1; i < dataArray.length; i++){
-# 	            var dateSetUp = dataArray[i].splice(0,3);
-# 	            var dateObject = new Date(dateSetUp);
-# 	            dataArray[i].unshift(dateObject);
-# 	          };
-# 	          return dataArray
-#       	};
+      		 for(var i=1; i < dataArray.length; i++){
+	            var dateSetUp = dataArray[i].splice(0,3);
+	            var dateObject = new Date(dateSetUp);
+	            dataArray[i].unshift(dateObject);
+	          };
+	          return dataArray
+      	};
 
-#         function drawChart() {
-#           var data = google.visualization.arrayToDataTable( formatDates() );
-#           var options = {
-#             title: 'Consolidated Burn Up',
-#             trendlines: { 1: {} }
-#           };
+        function drawChart() {
+          var data = google.visualization.arrayToDataTable( formatDates() );
+          var options = {
+            title: 'Consolidated Burn Up',
+            trendlines: { 1: {} }
+          };
 
-#           var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-#           chart.draw(data, options);
-#         }
-#       </script>
+          var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+          chart.draw(data, options);
+        }
+      </script>
 
-# 		}
+		}
 
-# end
+end
 
-# outfile = File.open('/Users/thoughtworker/Mingle/mingle_13_4_2/vendor/plugins/consolidated_burnup_chart/test.html', 'w')
-# outfile.puts('<HTML><head></head><body>')
-# outtext = render_chart(final_data)
-# outfile.puts(outtext)
-# outfile.close
+outfile = File.open('/Users/thoughtworker/Mingle/mingle_13_4_2/vendor/plugins/consolidated_burnup_chart/test.html', 'w')
+outfile.puts('<HTML><head></head><body>')
+outtext = render_chart
+outfile.puts(outtext)
+outfile.close
 
